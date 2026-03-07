fee53db0949ec6bbb4d37e3fcff32c64fd2574aa
0x0D <0xOD@proton.me>
2026-03-07 20:20:06 +0100

    feat(modules/base): add packages module

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./fonts.nix
     ./nix.nix
+    ./packages.nix
   ];
 }
---
+++ modules/base/packages.nix
@@ -0,0 +1,76 @@
+{ pkgs, ... }: {
+  environment.variables.EDITOR = "nvim --clean";
+
+  environment.systemPackages = with pkgs; [
+    # Core tools
+    fish
+    fastfetch
+    neovim
+    gnumake
+    git
+    git-lfs
+
+    # System monitoring
+    procs
+    btop
+
+    # Archives
+    zip
+    xz
+    zstd
+    unzipNLS
+    p7zip
+
+    # Text processing
+    gnugrep
+    gawk
+    gnutar
+    gnused
+    sad
+
+    # Networking tools
+    mtr
+    gping
+    dnsutils
+    ldns
+    doggo
+    wget
+    curl
+    curlie
+    httpie
+    aria2
+    socat
+    nmap
+    ipcalc
+    iperf3
+    hyperfine
+    tcpdump
+
+    # File transfer
+    rsync
+    croc
+
+    # Security
+    libargon2
+    openssl
+
+    # Misc
+    file
+    which
+    tree
+    tealdeer
+
+    # Other
+    jq
+    yq-go
+    jc
+    fzf
+    fd
+    findutils
+    ripgrep
+    duf
+    dust
+    gdu
+    ncdu
+  ];
+}
