055cbe8ecb74b11d2be2d0938b5fa98787770385
0x0D <0xOD@proton.me>
2026-03-07 22:07:12 +0100

    feat(hosts/iso): Added installer

--- hosts/iso/default.nix
+++ hosts/iso/default.nix
@@ -1,4 +1,80 @@
-{ config, lib, pkgs, nixos-hardware, ... }: {
+{ config, lib, pkgs, nixos-hardware, ... }: let
+  ifExists = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
+  install-system = pkgs.writeScriptBin "install-system" ''
+#!${pkgs.stdenv.shell}
+#set -euo pipefail
+
+TARGET_HOST="''${1:-}"
+TARGET_USER="''${2:-oxod}"
+
+if [ "$(id -u)" -eq 0 ]; then
+  echo "ERROR! $(basename "$0") should be run as a regular user"
+  exit 1
+fi
+
+if [ ! -d "$HOME/nix-config/.git" ]; then
+  git clone https://github.com/0x0Dx/nix-config.git "$HOME/nix-config"
+fi
+
+pushd "$HOME/nix-config"
+if [[ -z "$TARGET_HOST" ]]; then
+  echo "ERROR! $(basename "$0") requires a hostname as the first argument"
+  echo "       The following hosts are available"
+  ls -1 nixos/*/boot.nix | cut -d'/' -f2 | grep -v iso
+  exit 1
+fi
+
+if [[ -z "$TARGET_USER" ]]; then
+  echo "ERROR! $(basename "$0") requires a username as the second argument"
+  echo "       The following users are available"
+  ls -1 nixos/_mixins/users/ | grep -v -E "nixos|root"
+  exit 1
+fi
+
+if [ ! -e "nixos/$TARGET_HOST/disks.nix" ]; then
+  echo "ERROR! $(basename "$0") could not find the required nixos/$TARGET_HOST/disks.nix"
+  exit 1
+fi
+
+# Check if the machine we're provisioning expects a keyfile to unlock a disk.
+# If it does, generate a new key, and write to a known location.
+if grep -q "data.keyfile" "nixos/$TARGET_HOST/disks.nix"; then
+  echo -n "$(head -c32 /dev/random | base64)" > /tmp/data.keyfile
+fi
+
+echo "WARNING! The disks in $TARGET_HOST are about to get wiped"
+echo "         NixOS will be re-installed"
+echo "         This is a destructive operation"
+echo
+read -p "Are you sure? [y/N]" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  sudo true
+
+  sudo nix run github:nix-community/disko \
+    --extra-experimental-features "nix-command flakes" \
+    --no-write-lock-file \
+    -- \
+    --mode zap_create_mount \
+    "hosts/$TARGET_HOST/disks.nix"
+
+  sudo nixos-install --no-root-password --flake ".#$TARGET_HOST"
+
+  # Rsync nix-config to the target install and set the remote origin to SSH.
+  rsync -a --delete "$HOME/nix-config/" "/mnt/home/$TARGET_USER/nix-config/"
+  pushd "/mnt/home/$TARGET_USER/nix-config"
+  git remote set-url origin git@github.com:0x0Dx/nix-config.git
+  popd
+
+  # If there is a keyfile for a data disk, put copy it to the root partition and
+  # ensure the permissions are set appropriately.
+  if [[ -f "/tmp/data.keyfile" ]]; then
+    sudo cp /tmp/data.keyfile /mnt/etc/data.keyfile
+    sudo chmod 0400 /mnt/etc/data.keyfile
+  fi
+fi
+'';
+in {
   imports = [
     ./hardware-configuration.nix
 
@@ -16,6 +92,8 @@
     networkmanager.enable = false;
     useDHCP = false;
   };
+
+  config.environment.systemPackages = [ install-system ];
 
   networking.useNetworkd = true;
   systemd.network.enable = true;
