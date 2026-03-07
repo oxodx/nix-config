b752342f162d2f4e5af5fccf95b5fe0b9374a86e
0x0D <0xOD@proton.me>
2026-03-07 20:20:05 +0100

    chore(flake.nix): make substituters extra

--- flake.nix
+++ flake.nix
@@ -3,13 +3,13 @@
 
   outputs = inputs: import ./outputs inputs;
 
-  nix.settings = {
-    substituters = [
+  nixConfig = {
+    extra-substituters = [
       "https://cache.nixos.org/"
       "https://nix-gaming.cachix.org"
       "https://nix-community.cachix.org/"
     ];
-    trusted-public-keys = [
+    extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
