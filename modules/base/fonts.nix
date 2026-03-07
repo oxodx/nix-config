c134de6483cbf0f036f85006d2c711badcf8d3e8
0x0D <0xOD@proton.me>
2026-03-07 20:20:05 +0100

    feat(modules/base/fonts): update fonts configuration

--- modules/base/fonts.nix
+++ modules/base/fonts.nix
@@ -1,16 +1,24 @@
 { pkgs, ... }: {
-  fonts = {
-    enableDefaultFonts = true;
-    fonts = with pkgs; [
-      noto-fonts
-      noto-fonts-color-emoji
-      liberation_ttf
-      fira-code
-      fira-code-symbols
-      dina-font
-      proggyfonts
-      font-awesome_4
-      font-awesome_5
-    ];
-  };
+  fonts.packages = with pkgs; [
+    # Icon fonts
+    material-design-icons
+    font-awesome
+
+    # Nerd fonts
+    nerd-fonts.symbols-only
+    nerd-fonts.fira-code
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.iosevka
+
+    # Noto fonts
+    noto-fonts
+    noto-fonts-color-emoji
+
+    # Source Fonts
+    source-sans
+    source-serif
+    source-han-sans
+    source-han-serif
+    source-han-mono
+  ];
 }
