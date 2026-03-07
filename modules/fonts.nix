fc45980bea23e49296bf419b3b4290a4a453732c
0x0D <0xOD@proton.me>
2026-03-07 20:20:05 +0100

    fix(modules/base/fonts): fix fonts configuration

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -3,14 +3,10 @@
     enableDefaultFonts = true;
     fonts = with pkgs; [
       noto-fonts
-      noto-fonts
-      noto-fonts-cjk
-      noto-fonts-emoji
-      noto-fonts-extra
+      noto-fonts-color-emoji
       liberation_ttf
       fira-code
       fira-code-symbols
-      mplus-outline-fonts
       dina-font
       proggyfonts
       font-awesome_4
