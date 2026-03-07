31ad83a35a0d41506941a0acce6f69299f49f03d
0x0D <0xOD@proton.me>
2026-03-07 20:20:05 +0100

    refactor(modules): rename nix-binary-cache to binary-caches

--- modules/nix-binary-caches.nix
+++ modules/nix-binary-caches.nix
@@ -1,11 +1,11 @@
 {
-  config.nix = {
-    binaryCaches = [
+  config.nix.settings = {
+    substituters = [
       "https://cache.nixos.org/"
       "https://nix-gaming.cachix.org"
       "https://nix-community.cachix.org/"
     ];
-    binaryCachePublicKeys = [
+    trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
