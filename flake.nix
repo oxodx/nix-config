25699a40759b92967f38cf0765463d0bcfdad5ab
0x0D <0xOD@proton.me>
2026-03-07 20:20:05 +0100

    refactor(flake): move outputs to separate folder

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   description = "0x0D's nix configuration";
 
+  outputs = inputs: import ./outputs inputs;
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixos-hardware.url = "github:0x0Dx/nixos-hardware";
@@ -15,41 +17,4 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
-  outputs = { self, nixpkgs, home-manager, nix-gaming, ... }@args:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config = { allowUnfree = true; };
-      };
-      lib = nixpkgs.lib;
-      readModules = path: builtins.map (x: path + "/${x}") (builtins.attrNames (builtins.readDir path));
-      makeHost = path: lib.nixosSystem {
-        inherit system;
-
-        specialArgs = {
-          nixos-hardware = "${args.nixos-hardware}";
-        };
-
-        modules = [
-          { nixpkgs.pkgs = pkgs; }
-          home-manager.nixosModules.home-manager
-          nix-gaming.nixosModules.pipewireLowLatency
-          path
-          ./users
-          {
-            config = {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-            };
-          }
-        ] ++ (readModules ./modules);
-      };
-    in
-    {
-      nixosConfigurations = {
-        kumquat = makeHost ./hosts/kumquat;
-      };
-    };
 }
---
+++ outputs/default.nix
@@ -0,0 +1,37 @@
+{ self, nixpkgs, home-manager, nix-gaming, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config = { allowUnfree = true; };
+      };
+      lib = nixpkgs.lib;
+      readModules = path: builtins.map (x: path + "/${x}") (builtins.attrNames (builtins.readDir path));
+      makeHost = path: lib.nixosSystem {
+        inherit system;
+
+        specialArgs = {
+          nixos-hardware = "${args.nixos-hardware}";
+        };
+
+        modules = [
+          { nixpkgs.pkgs = pkgs; }
+          home-manager.nixosModules.home-manager
+          nix-gaming.nixosModules.pipewireLowLatency
+          path
+          ./users
+          {
+            config = {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+            };
+          }
+        ] ++ (readModules ./modules);
+      };
+    in
+    {
+      nixosConfigurations = {
+        kumquat = makeHost ./hosts/kumquat;
+      };
+    }
+    
