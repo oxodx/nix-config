ea1ed4cf8c5d30956d72786691f06580bae40f3e
0x0D <0xOD@proton.me>
2026-03-07 20:20:05 +0100

    fix(flake.nix): add missing comma

--- outputs/default.nix
+++ outputs/default.nix
@@ -1,4 +1,4 @@
-{ self, nixpkgs, home-manager, nix-gaming ... }@inputs:
+{ self, nixpkgs, home-manager, nix-gaming, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
