b53dc71f1263875bf06be5ad154e2cb38b3de781
0x0D <0xOD@proton.me>
2026-03-08 09:36:09 +0100

    fix: pass path argument to scanPaths

--- lib/default.nix
+++ lib/default.nix
@@ -1,20 +1,18 @@
-{ lib, ... }: {
+{ lib, ... }:
+{
   nixosSystem = import ./nixosSystem.nix;
 
   attrs = import ./attrs.nix { inherit lib; };
 
   relativeToRoot = lib.path.append ../.;
-  scanPaths = paths: builtins.map (f: (path + "/${f}")) (
-    builtins.attrNames (
-      lib.attrsets.filterAttrs
-        (
-          path: _type: (_type == "directory")
-            || (
-            (path != "default.nix")
-              && (lib.strings.hasSuffix ".nix" path)
-          )
-        )
-        (builtins.readDir path)
-    )
-  );
+  scanPaths =
+    path:
+    builtins.map (f: (path + "/${f}")) (
+      builtins.attrNames (
+        lib.attrsets.filterAttrs (
+          path: _type:
+          (_type == "directory") || ((path != "default.nix") && (lib.strings.hasSuffix ".nix" path))
+        ) (builtins.readDir path)
+      )
+    );
 }
