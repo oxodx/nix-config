6e0ed8999f86ae39fa59fa0be7043b2457979416
0x0D <0xOD@proton.me>
2026-03-08 13:42:07 +0100

    refactor: restructure user and group configuration in user-group.nix
    Updated the user and group definitions to utilize a more modular approach, allowing for dynamic user creation based on configuration. Retained existing groups and root user setup while enhancing clarity and maintainability.

--- modules/nixos/base/user-group.nix
+++ modules/nixos/base/user-group.nix
@@ -1,23 +1,41 @@
+{ lib, config, ... }:
+with lib;
+let
+  cfg = config.modules.base.users;
+in
 {
-  users.mutableUsers = false;
-
-  users.groups = {
-    "oxod" = { };
-    podman = { };
-    wireshark = { };
-    adbusers = { };
-    dialout = { };
-    plugdev = { };
-    uinput = { };
+  options.modules.base.users = {
+    users = mkOption {
+      type = with types; listOf str;
+      default = [ ];
+      description = "Normal users to create on this host.";
+    };
   };
 
-  users.users = {
-    oxod = {
+  config = {
+    users.mutableUsers = false;
+
+    users.groups = {
+      podman = { };
+      wireshark = { };
+      adbusers = { };
+      dialout = { };
+      plugdev = { };
+      uinput = { };
+    }
+    // (genAttrs cfg.users (name: { }));
+
+    users.users = {
+      root = {
+        initialPassword = "placeholder";
+      };
+    }
+    // (genAttrs cfg.users (name: {
       initialPassword = "placeholder";
-      home = "/home/oxod";
+      home = "/home/${name}";
       isNormalUser = true;
       extraGroups = [
-        "oxod"
+        name
         "users"
         "wheel"
         "networkmanager"
@@ -26,10 +44,6 @@
         "adbusers"
         "libvirtd"
       ];
-    };
-
-    root = {
-      initialPassword = "placeholder";
-    };
+    }));
   };
 }
