54b79028ca2c0770c05ba15019eb777a2a5b2f72
0x0D <0xOD@proton.me>
2026-03-08 09:49:48 +0100

    fix: add allowUnfree to nixpkgs config

--- lib/nixosSystem.nix
+++ lib/nixosSystem.nix
@@ -1,4 +1,13 @@
-{ inputs, lib, system, genSpecialArgs, nixos-modules, home-modules ? [ ], specialArgs ? (genSpecialArgs system), ... }:
+{
+  inputs,
+  lib,
+  system,
+  genSpecialArgs,
+  nixos-modules,
+  home-modules ? [ ],
+  specialArgs ? (genSpecialArgs system),
+  ...
+}:
 let
   inherit (inputs) nixpkgs home-manager nixos-generators;
 in
@@ -8,6 +17,7 @@ nixpkgs.lib.nixosSystem {
     nixos-modules
     ++ [
       nixos-generators.nixosModules.all-formats
+      { nixpkgs.config.allowUnfree = true; }
     ]
     ++ (lib.optionals ((lib.lists.length home-modules) > 0) [
       home-manager.nixosModules.home-manager
