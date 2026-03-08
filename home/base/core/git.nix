0f88e65be15ccb94fdff1df970a33f9b440cfa68
0x0D <0xOD@proton.me>
2026-03-08 08:31:56 +0100

    feat: add git home-manager module

---
+++ home/base/core/git.nix
@@ -0,0 +1,84 @@
+{ config, lib, pkgs, ... }: {
+  home.activation.removeExistingGitconfig = lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
+    rm -f ${config.home.homeDirectory}/.gitconfig
+  '';
+
+  programs.gh = {
+    enable = true;
+    settings = {
+      git_protocol = "ssh";
+      prompt = "enabled";
+      aliases = {
+        co = "pr checkout";
+        pv = "pr view";
+      };
+    };
+    hosts = {
+      "github.com" = {
+        "users" = {
+          "0x0Dx" = null;
+        };
+        "user" = "0x0Dx";
+      };
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+
+    settings = {
+      user.email = "0xOD@proton.me";
+      user.name = "0x0D";
+
+      init.defaultBranch = "main";
+      trim.bases = "develop,master,main";
+      push.autoSetupRemote = true;
+      pull.rebase = true;
+      log.date = "iso";
+
+      url = {
+        "ssh://git@github.com/0x0Dx" = {
+          insteadOf = "https://github.com/0x0Dx";
+        };
+      };
+
+      alias = {
+        br = "branch";
+        co = "checkout";
+        st = "status";
+        ls = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate";
+        ll = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate --numstat";
+        cm = "commit -m";
+        ca = "commit -am";
+        dc = "diff --cached";
+
+        amend = "commit --amend -m";
+        unstage = "reset HEAD --";
+        merged = "branch --merged";
+        unmerged = "branch --no-merged";
+        nonexist = "remote prune origin --dry-run";
+
+        delmerged = ''! git branch --merged | egrep -v "(^\*|main|master|dev|staging)" | xargs git branch -d'';
+        delnonexist = "remote prune origin";
+
+        update = "submodule update --init --recursive";
+        foreach = "submodule foreach";
+      };
+    };
+  };
+
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      diff-so-fancy = true;
+      line-numbers = true;
+      true-color = "always";
+    };
+  };
+
+  programs.lazygit.enable = true;
+
+  programs.gitui.enable = false;
+}
