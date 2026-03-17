e0b07fd38c954352b3e013b46ef0a474095199b2
0x0D <0xOD@proton.me>
2026-03-17 22:37:08 +0100

    feat: Add K3s, KubeVirt and Colmena support
    - Add disko and preservation flake inputs
    - Add lib functions for generating K3s server/agent and KubeVirt modules
    - Add colmena support for remote deployment via SSH
    - Add k8s host configuration with k3s-master-1 and kubevirt-homelab

--- lib/default.nix
+++ lib/default.nix
@@ -1,8 +1,14 @@
 { lib, ... }:
 {
+  colmenaSystem = import ./colmenaSystem.nix;
   nixosSystem = import ./nixosSystem.nix;
 
   attrs = import ./attrs.nix { inherit lib; };
+
+  genK3sServerModule = import ./genK3sServerModule.nix;
+  genK3sAgentModule = import ./genK3sAgentModule.nix;
+  genKubeVirtHostModule = import ./genKubeVirtHostModule.nix;
+  genKubeVirtGuestModule = import ./genKubeVirtGuestModule.nix;
 
   relativeToRoot = lib.path.append ../.;
   scanPaths =
---
+++ outputs/x86_64-linux/src/kubevirt-homelab.nix
@@ -0,0 +1,36 @@
+{
+  # NOTE: the args not used in this file CAN NOT be removed!
+  # because haumea pass argument lazily,
+  # and these arguments are used in the functions like `mylib.nixosSystem`, `mylib.colmenaSystem`, etc.
+  inputs,
+  lib,
+  mylib,
+  system,
+  genSpecialArgs,
+  ...
+}@args:
+let
+  name = "kubevirt-homelab";
+  tags = [
+    name
+    "virt-homelab"
+  ];
+  ssh-user = "root";
+
+  modules = {
+    nixos-modules =
+      (map mylib.relativeToRoot [
+        "modules/nixos/server/server.nix"
+        "hosts/k8s/${name}"
+      ]);
+  };
+
+  systemArgs = modules // args;
+in
+{
+  nixosConfigurations.${name} = mylib.nixosSystem systemArgs;
+
+  colmena.${name} = mylib.colmenaSystem (systemArgs // { inherit tags ssh-user; });
+
+  packages.${name} = inputs.self.nixosConfigurations.${name}.config.formats.iso;
+}
