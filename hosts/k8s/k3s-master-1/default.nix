ec1d14f10bf3e36a36f4b80a3032ad3a5ed12447
0x0D <0xOD@proton.me>
2026-03-17 22:40:07 +0100

    refactor: Remove unused myvars references from K8s hosts

--- hosts/k8s/k3s-master-1/default.nix
+++ hosts/k8s/k3s-master-1/default.nix
@@ -1,7 +1,6 @@
 {
   config,
   pkgs,
-  myvars,
   mylib,
   ...
 }:
@@ -10,17 +9,15 @@ let
 
   coreModule = mylib.genKubeVirtGuestModule {
     inherit pkgs hostName;
-    inherit (myvars) networking;
   };
   k3sModule = mylib.genK3sServerModule {
     inherit pkgs;
     kubeconfigFile = "/home/${myvars.username}/.kube/config";
-    tokenFile = config.age.secrets."k3s-test-1-token".path;
     # the first node in the cluster should be the one to initialize the cluster
     clusterInit = true;
     # use my own domain & kube-vip's virtual IP for the API server
     # so that the API server can always be accessed even if some nodes are down
-    masterHost = "test-cluster-1.writefor.fun";
+    masterHost = "cluster-1.local";
 
     # k3sExtraArgs = [
     #   # IPv4 Private CIDR(full) - 172.16.0.0/12
