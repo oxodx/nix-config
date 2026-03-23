6151c605b4d55344b556a27caa56e6ddde8de112
0x0D <0xOD@proton.me>
2026-03-23 11:21:11 +0100

    refactor(home): consolidate host home modules under home/hosts

--- home/hosts/linux/kumquat.nix
+++ home/hosts/linux/kumquat.nix
@@ -3,6 +3,12 @@ let
   mkSymlink = config.lib.file.mkOutOfStoreSymlink;
 in
 {
+  imports = [ ../../linux/gui.nix ];
+
+  programs.ssh.matchBlocks."github.com".identityFile = "${config.home.homeDirectory}/.ssh/kumquat";
+
+  modules.desktop.gaming.enable = true;
+  modules.desktop.niri.enable = true;
   modules.desktop.nvidia.enable = true;
 
   xdg.configFile."niri/niri-hardware.kdl".source =
