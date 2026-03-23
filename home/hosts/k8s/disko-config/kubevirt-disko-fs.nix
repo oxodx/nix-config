0cbd00ad44edf05d905a92e6e757cb3034df80e9
0x0D <0xOD@proton.me>
2026-03-23 11:21:11 +0100

    refactor(home): consolidate host home modules under home/hosts

---
+++ home/hosts/k8s/disko-config/kubevirt-disko-fs.nix
@@ -0,0 +1,82 @@
+{
+  # required by preservation
+  fileSystems."/persistent".neededForBoot = true;
+  disko.devices = {
+    nodev."/" = {
+      fsType = "tmpfs";
+      mountOptions = [
+        "size=4G"
+        "defaults"
+        "mode=755"
+      ];
+    };
+    disk.main = {
+      type = "disk";
+      device = "/dev/nvme0n1";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "630M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [
+                "fmask=0177"
+                "dmask=0077"
+                "noexec,nosuid,nodev"
+              ];
+            };
+          };
+          root = {
+            size = "100%";
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-f" ];
+              subvolumes = {
+                "/" = {
+                  mountpoint = "/btr_pool";
+                  mountOptions = [ "subvolid=5" ];
+                };
+                "@nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = [
+                    "compress-force=zstd:1"
+                    "noatime"
+                  ];
+                };
+                "@persistent" = {
+                  mountpoint = "/persistent";
+                  mountOptions = [
+                    "compress-force=zstd:1"
+                    "noatime"
+                  ];
+                };
+                "@tmp" = {
+                  mountpoint = "/tmp";
+                  mountOptions = [
+                    "compress-force=zstd:1"
+                    "noatime"
+                  ];
+                };
+                "@snapshots" = {
+                  mountpoint = "/snapshots";
+                  mountOptions = [
+                    "compress-force=zstd:1"
+                    "noatime"
+                  ];
+                };
+                "@swap" = {
+                  mountpoint = "/swap";
+                  swap.swapfile.size = "16384M";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
