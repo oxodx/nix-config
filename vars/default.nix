1c6caae73af1df65574c47a5bd4949075af74181
0x0D <0xOD@proton.me>
2026-03-23 15:09:24 +0100

    chore: Add password instead of a placeholder password

--- vars/default.nix
+++ vars/default.nix
@@ -3,4 +3,10 @@
   username = "oxod";
   userfullname = "0x0D";
   useremail = "0xOD@proton.me";
+  # Generated using: mkpasswd -m yescrypt --rounds=11
+  # Password: long, strong random string (full charset)
+  # Rotation policy: changed annually
+  # Purpose: system login password only
+  # https://man.archlinux.org/man/crypt.5.en
+  initialHashedPassword = "$y$jFT$O6/xS4HtgDk15dQB483T11$HU1zvzxucp/C.YiLM8brR68w39Qm.HwdVc5BXpOfO1B";
 }
