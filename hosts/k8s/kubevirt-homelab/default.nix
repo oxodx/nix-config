74cf9c2f59c8fcb5f40b00bb40645abbf7aca6f7
0x0D <0xOD@proton.me>
2026-03-23 13:56:40 +0100

    chore: Added networkmanager to homelab

--- hosts/k8s/kubevirt-homelab/default.nix
+++ hosts/k8s/kubevirt-homelab/default.nix
@@ -44,6 +44,13 @@ in
     k3sModule
   ];
 
+  networking = {
+    hostName = "kubevirt-homelab";
+
+    networkmanager.enable = true;
+    useDHCP = false;
+  };
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
   boot.loader.efi.canTouchEfiVariables = true;
