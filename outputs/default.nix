d88282f720159202699d73176b9032ab9218151a
0x0D <0xOD@proton.me>
2026-03-23 16:21:07 +0100

    chore: nixfmt-rfc-style got migrated to pkgs.nixfmt

--- outputs/default.nix
+++ outputs/default.nix
@@ -95,7 +95,7 @@ in
     pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
       src = mylib.relativeToRoot ".";
       hooks = {
-        nixfmt-rfc-style = {
+        pkgs.nixfmt = {
           enable = true;
           settings.width = 100;
         };
