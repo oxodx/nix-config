5f729a748ec9212a19646925efa50e14437361f1
0x0D <0xOD@proton.me>
2026-03-24 22:32:38 +0100

    fix: Please work now

--- hosts/homelab/oci-containers/default.nix
+++ hosts/homelab/oci-containers/default.nix
@@ -6,33 +6,27 @@
 {
   imports = mylib.scanPaths ./.;
 
+  # Bind mount /var/lib/containers to /data/apps/podman
+  fileSystems."/var/lib/containers" = {
+    device = "/data/apps/podman";
+    options = [ "bind" ];
+  };
+
+  # Ensure the directory exists before mounting
   systemd.tmpfiles.rules = [
-    "d /data/apps/podman          0755 root root -"
+    "d /data/apps/podman 0755 root root -"
   ];
 
   virtualisation = {
     docker.enable = lib.mkForce false;
     podman = {
       enable = true;
-
-      # Create a docker alias for podman
       dockerCompat = true;
-
-      # Required for containers under podman-compose to be able to talk to each other
       defaultNetwork.settings.dns_enabled = true;
-
-      # Periodically prune Podman resources
       autoPrune = {
         enable = true;
         dates = "weekly";
         flags = [ "--all" ];
-      };
-
-      # Configure the storage location
-      containersConf = {
-        engine = {
-          volume_path = "/data/apps/podman/volumes";
-        };
       };
     };
 
