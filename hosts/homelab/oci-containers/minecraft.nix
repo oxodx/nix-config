ce17ff691be0ed669badff3a2a14b98a681527b6
0x0D <0xOD@proton.me>
2026-03-24 21:11:56 +0100

    fix: Oh damn i messed up wrong file

--- hosts/homelab/oci-containers/minecraft.nix
+++ hosts/homelab/oci-containers/minecraft.nix
@@ -1,12 +1,38 @@
 {
   lib,
-  config,
+  pkgs,
   ...
 }:
 let
   user = "minecraft";
   uid = 990;
   dataDir = "/data/apps/minecraft/mc-1";
+
+  # TODO: replace with proper secret management
+  rconPassword = "changeme";
+
+  lazymcToml = pkgs.writeText "lazymc.toml" ''
+    [server]
+    address = "0.0.0.0:25565"
+    directory = "/data"
+    start_on_join = true
+    wake_on_start = true
+    freeze_process = false
+
+    [public]
+    address = "0.0.0.0:25565"
+    version = "1.21.1"
+    protocol = 767
+
+    [rcon]
+    enabled = true
+    host = "mc"
+    port = 25575
+    password = "${rconPassword}"
+
+    [time]
+    sleep_after = 300
+  '';
 in
 {
   users.groups.${user} = { };
@@ -24,6 +50,21 @@ in
     "Z ${dataDir}                  0755 ${user} ${user} -"
   ];
 
+  virtualisation.oci-containers.containers."minecraft-lazymc" = {
+    image = "ghcr.io/timvisee/lazymc:latest";
+    volumes = [
+      "${lazymcToml}:/lazymc.toml:ro"
+    ];
+    ports = [
+      "25565:25565/tcp"
+    ];
+    log-driver = "journald";
+    extraOptions = [
+      "--network=minecraft_default"
+      "--no-healthcheck"
+    ];
+  };
+
   virtualisation.oci-containers.containers."minecraft-mc" = {
     image = "itzg/minecraft-server:latest";
     user = "${toString uid}:${toString uid}";
@@ -35,6 +76,8 @@ in
       "MEMORY" = "2048M";
       "OPS" = "0x0D_
   jaydon30";
+      "RCON_ENABLED" = "true";
+      "RCON_PASSWORD" = rconPassword;
       "SIMULATION_DISTANCE" = "6";
       "TYPE" = "PAPER";
       "USE_MEOWICE_FLAGS" = "true";
@@ -45,9 +88,6 @@ in
     };
     volumes = [
       "${dataDir}:/data:rw"
-    ];
-    ports = [
-      "25565:25565/tcp"
     ];
     log-driver = "journald";
     extraOptions = [
