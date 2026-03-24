e0dd05bcf8587154c0730820278fe7cc2a5da74b
0x0D <0xOD@proton.me>
2026-03-24 21:20:49 +0100

    fix: Use the docker socket

--- hosts/homelab/oci-containers/default.nix
+++ hosts/homelab/oci-containers/default.nix
@@ -12,6 +12,7 @@
       enable = true;
       # Create a `docker` alias for podman, to use it as a drop-in replacement
       dockerCompat = true;
+      dockerSocket.enable = true;
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
       # Periodically prune Podman resources
