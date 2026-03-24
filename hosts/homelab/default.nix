b6fbcc83b3bdc6c9e78b829d6234773868e596f5
0x0D <0xOD@proton.me>
2026-03-24 12:03:58 +0100

    feat: Removed preservation from homelab and added Caddy to homelab

--- hosts/homelab/default.nix
+++ hosts/homelab/default.nix
@@ -12,10 +12,12 @@ in
 {
   imports = (mylib.scanPaths ./.) ++ [
     disko.nixosModules.default
-    ./disko.nix
-    ./hardware-configuration.nix
-    ./preservation.nix
   ];
+
+  # Maximum total amount of memory that can be stored in the zram swap devices (as a percentage of your total memory).
+  # Defaults to 1/2 of your total RAM. Run zramctl to check how good memory is compressed.
+  # This doesn’t define how much memory will be used by the zram swap devices.
+  zramSwap.memoryPercent = lib.mkForce 100;
 
   networking = {
     hostName = "kumquat";
