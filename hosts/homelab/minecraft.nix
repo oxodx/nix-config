b8cdc05c6b83cbe621bb24bd1628742ee4d2e1d0
0x0D <0xOD@proton.me>
2026-03-24 14:17:10 +0100

    feat: Added minecraft server yipee

---
+++ hosts/homelab/minecraft.nix
@@ -0,0 +1,30 @@
+{
+  pkgs,
+  ...
+}:
+{
+  # https://github.com/NixOS/nixpkgs/blob/nixos-25.11/nixos/modules/services/games/minecraft-server.nix
+  services.minecraft-server = {
+    enable = true;
+    eula = true;
+    declarative = true;
+
+    package = pkgs.minecraft-server-1-21-11;
+    stateDir = "/data/apps/minecraft/mc-1";
+
+    serverProperties = {
+      gamemode = "survival";
+      difficulty = "hard";
+      simulation-distance = 10;
+      level-seed = "4";
+      white-list = true;
+    };
+
+    whitelist = {
+      "0x0D_" = "ef9ebb67-034c-4520-a700-6c67a3d63bb1";
+      "jaydon30" = "52566ea2-67eb-4266-8a9a-ad10ca6ec0df";
+    };
+
+    jvmOpts = "-Xms4092M -Xmx4092M -XX:+UseG1GC";
+  };
+}
