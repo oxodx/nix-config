db3e6612ea8b039e89fdd2988482d19666a5ba54
0x0D <0xOD@proton.me>
2026-03-24 21:29:45 +0100

    chore: Switch to docker cuz podman is anoying

--- hosts/homelab/oci-containers/default.nix
+++ hosts/homelab/oci-containers/default.nix
@@ -7,26 +7,17 @@
   imports = mylib.scanPaths ./.;
 
   virtualisation = {
-    docker.enable = lib.mkForce false;
-    podman = {
+    docker = {
       enable = true;
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-      dockerSocket.enable = true;
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
-      # Periodically prune Podman resources
       autoPrune = {
         enable = true;
         dates = "weekly";
         flags = [ "--all" ];
       };
     };
-
+    podman.enable = lib.mkForce false;
     oci-containers = {
-      backend = "podman";
+      backend = "docker";
     };
   };
-
-  systemd.sockets.podman.enable = true;
 }
