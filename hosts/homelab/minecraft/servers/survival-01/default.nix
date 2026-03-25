e1ca9f392cd56ee2161835068aba63b86f55337f
0x0D <0xOD@proton.me>
2026-03-25 21:53:26 +0100

    refactor: Formatted it nicely

--- hosts/homelab/minecraft/servers/survival-01/default.nix
+++ hosts/homelab/minecraft/servers/survival-01/default.nix
@@ -1,21 +1,21 @@
 { pkgs, ... }:
+let
+  serverVersion = "1_21_11";
+in
 {
   services.minecraft-servers.servers.survival = {
     enable = true;
     enableReload = true;
-    package = pkgs.paperServers.paper-1_21_11;
+
+    package = pkgs.paperServers."paper-${serverVersion}";
     jvmOpts = ((import ../../aikar-flags.nix) "2G") + "-Dpaper.disableChannelLimit=true";
     whitelist = import ./whitelist.nix;
     serverProperties = {
       server-port = 25581;
-      max-tick-time = -1;
       online-mode = false;
+      max-tick-time = -1;
     };
-    files = {
-      "plugins/ViaVersion/config.yml".value = {
-        checkforupdates = false;
-      };
-    };
+
     symlinks = {
       "plugins/ViaVersion.jar" = pkgs.fetchurl rec {
         pname = "ViaVersion";
@@ -30,6 +30,13 @@
         hash = "sha256:cbf4ce4dc132b1bcccc2abb9626ed18bb1bd116c9300657543458241b01000f7";
       };
     };
+
+    files = {
+      "plugins/ViaVersion/config.yml".value = {
+        checkforupdates = false;
+      };
+    };
+
     lazymc = {
       enable = true;
       config = {
