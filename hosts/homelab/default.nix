ec9de762e81dae6b18361ce3b68908dcf58934f5
0x0D <0xOD@proton.me>
2026-03-26 10:57:53 +0100

    chore: Maybe now use static ip

--- hosts/homelab/default.nix
+++ hosts/homelab/default.nix
@@ -6,42 +6,36 @@
   disko,
   ...
 }:
-let
-  hostName = "homelab";
-in
 {
   imports = (mylib.scanPaths ./.) ++ [
     disko.nixosModules.default
   ];
 
-  # Maximum total amount of memory that can be stored in the zram swap devices (as a percentage of your total memory).
-  # Defaults to 1/2 of your total RAM. Run zramctl to check how good memory is compressed.
-  # This doesn’t define how much memory will be used by the zram swap devices.
   zramSwap.memoryPercent = lib.mkForce 100;
 
   networking = {
     hostName = "homelab";
+    useDHCP = false;
+
+    interfaces.wlan0 = {
+      useDHCP = false;
+      ipv4.addresses = [
+        {
+          address = "192.168.1.184";
+          prefixLength = 24;
+        }
+      ];
+    };
+
+    defaultGateway = "192.168.1.1";
+    nameservers = [
+      "1.1.1.1"
+      "8.8.8.8"
+    ];
 
     networkmanager = {
       enable = true;
-      dhcp = false;
       wifi.powersave = lib.mkDefault 2;
-
-      connections = {
-        # USB WiFi Dongle - Static IP
-        "wlan0" = {
-          ipv4.addresses = [
-            {
-              address = "192.168.1.184/24";
-              gateway = "192.168.1.1";
-            }
-          ];
-          ipv4.dns = [
-            "1.1.1.1"
-            "8.8.8.8"
-          ];
-        };
-      };
     };
   };
 
