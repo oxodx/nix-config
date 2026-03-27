bff8ae2e80a3ae9a19a81990fd31ffe97e216d9d
0x0D <0xOD@proton.me>
2026-03-27 13:08:56 +0100

    feat: Added even more optimization stuff and added the spark plugin

--- hosts/homelab/minecraft/servers/survival01/default.nix
+++ hosts/homelab/minecraft/servers/survival01/default.nix
@@ -16,10 +16,16 @@ in
       max-tick-time = -1;
       network-compression-threshold = 256;
       simulation-distance = 4;
-      view-distance = 7;
+      view-distance = 8;
     };
 
     symlinks = {
+      "plugins/Spark.jar" = pkgs.fetchurl rec {
+        pname = "spark";
+        version = "1.10.172";
+        url = "https://ci.lucko.me/job/${pname}/524/artifact/${pname}-bukkit/build/libs/${pname}-${version}-bukkit.jar";
+        hash = "sha256:4a37b57559f00c6eea84ee9f026316135b45312748676a1b2640b13c2b844cbd";
+      };
       "plugins/LuckPerms.jar" = pkgs.fetchurl rec {
         pname = "LuckPerms";
         version = "5.5.17";
@@ -50,22 +56,22 @@ in
       "config/paper-world-defaults.yml".value = {
         despawn-ranges = {
           ambient = {
-            hard = 72;
-            soft = 30;
+            hard = 128;
+            soft = 32;
+          };
+          creature = {
+            hard = 128;
+            soft = 32;
+          };
+          monster = {
+            hard = 128;
+            soft = 32;
           };
           axolotls = {
             hard = 72;
             soft = 30;
           };
-          creature = {
-            hard = 72;
-            soft = 30;
-          };
           misc = {
-            hard = 72;
-            soft = 30;
-          };
-          monster = {
             hard = 72;
             soft = 30;
           };
@@ -82,6 +88,71 @@ in
             soft = 30;
           };
         };
+        per-player-mob-spawns = true;
+        max-entity-collisions = 2;
+        update-pathfinding-on-block-update = false;
+        fix-climbing-bypassing-cramming-rule = true;
+        armor-stands.tick = false;
+        armor-stands.do-collision-entity-lookups = false;
+        tick-rates = {
+          behavior.villager = {
+            validatenearbypoi = 60;
+            acquirepoi = 120;
+          };
+          sensor.villager = {
+            secondarypoisensor = 80;
+            nearestbedsensor = 80;
+            villagerbabiessensor = 40;
+            playersensor = 40;
+            nearestlivingentitysensor = 40;
+          };
+        };
+        alt-item-despawn-rate = {
+          enabled = true;
+          items = {
+            cobblestone = 600;
+            netherrack = 600;
+            sand = 600;
+            red_sand = 600;
+            gravel = 600;
+            dirt = 600;
+            short_grass = 600;
+            pumpkin = 600;
+            melon_slice = 600;
+            kelp = 600;
+            bamboo = 600;
+            sugar_cane = 600;
+            twisting_vines = 600;
+            weeping_vines = 600;
+            oak_leaves = 600;
+            spruce_leaves = 600;
+            birch_leaves = 600;
+            jungle_leaves = 600;
+            acacia_leaves = 600;
+            dark_oak_leaves = 600;
+            mangrove_leaves = 600;
+            cherry_leaves = 600;
+            cactus = 600;
+            diorite = 600;
+            granite = 600;
+            andesite = 600;
+            scaffolding = 600;
+          };
+        };
+        redstone-implementation = "ALTERNATE_CURRENT";
+        "hopper.disable-move-event" = true;
+        "hopper.ignore-occluding-blocks" = true;
+        "tick-rates.mob-spawner" = 2;
+        optimize-explosions = true;
+        "treasure-maps.enabled" = true;
+        "treasure-maps.find-already-discovered" = {
+          loot-tables = true;
+          villager-trade = true;
+        };
+        "tick-rates.grass-spread" = 8;
+        "tick-rates.container-update" = 2;
+        non-player-arrow-despawn-rate = 20;
+        creative-arrow-despawn-rate = 20;
       };
       "spigot.yml".value = {
         view-distance = "default";
@@ -114,12 +185,12 @@ in
         tick-inactive-villagers = false;
         nerf-spawner-mobs = true;
         entity-activation-range = {
-          animals = 16;
-          monsters = 24;
+          animals = 32;
+          monsters = 32;
+          villagers = 32;
+          misc = 16;
+          water = 24;
           raiders = 48;
-          misc = 8;
-          water = 8;
-          villagers = 16;
           flying-monsters = 48;
         };
         entity-tracking-range = {
@@ -129,16 +200,22 @@ in
           misc = 32;
           other = 64;
         };
+        merge-radius = {
+          item = 3.5;
+          exp = 4.0;
+        };
+        hopper-transfer = 8;
+        hopper-check = 8;
       };
       "bukkit.yml".value = {
         spawn-limits = {
-          monsters = 20;
-          animals = 5;
-          water-animals = 2;
-          water-ambient = 2;
+          monsters = 50;
+          animals = 15;
+          water-animals = 8;
+          water-ambient = 8;
           water-underground-creature = 3;
-          axolotls = 3;
-          ambient = 1;
+          axolotls = 5;
+          ambient = 3;
         };
         ticks-per = {
           monster-spawns = 10;
