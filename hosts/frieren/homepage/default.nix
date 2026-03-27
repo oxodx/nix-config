657b0bd2c8c6da8e3cb85215c09a4f1e9e3e08e7
0x0D <0xOD@proton.me>
2026-03-27 22:12:16 +0100

    refactor: Split settings into its own file

--- hosts/frieren/homepage/default.nix
+++ hosts/frieren/homepage/default.nix
@@ -4,38 +4,7 @@
     allowedHosts = "home.oxod.nl";
 
     # https://gethomepage.dev/configs/settings/
-    settings = {
-      title = "0x0D's Homelab";
-      baseUrl = "http://home.oxod.nl";
-      startUrl = "http://home.oxod.nl";
-      favicon = "https://oxod.nl/favicon.ico";
-      useEqualHeights = true;
-      headerStyle = "clean";
-      theme = "dark";
-      color = "slate";
-      language = "en";
-      layout = {
-        "Homelab Monitoring" = {
-          icon = "mdi-monitor-dashboard";
-          initiallyCollapsed = false;
-          tab = "Main";
-          style = "row";
-          columns = 3;
-        };
-        "Homelab Applications" = {
-          icon = "si-homepage";
-          tab = "Main";
-        };
-      };
-      quicklaunch = {
-        searchDescriptions = true;
-        hideInternetSearch = true;
-        showSearchSuggestions = true;
-        hideVisitURL = true;
-      };
-      showStats = true;
-      hideErrors = false;
-    };
+    settings = import ./settings.nix;
 
     # https://gethomepage.dev/configs/bookmarks/
     bookmarks = [ ];
