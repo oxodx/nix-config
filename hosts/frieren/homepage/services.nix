9be35fab8bad2393be2fb60cbd83837c7d97c203
0x0D <0xOD@proton.me>
2026-03-27 23:31:03 +0100

    chore: Added AdGuardHome and UptimeKuma to homepage

--- hosts/frieren/homepage/services.nix
+++ hosts/frieren/homepage/services.nix
@@ -9,6 +9,22 @@
           siteMonitor = "http://glances.oxod.nl";
         };
       }
+      {
+        AdGuardHome = {
+          icon = "adguard-home.svg";
+          href = "http://adguard.oxod.nl";
+          description = "Network-wide ads & trackers blocking DNS server";
+          siteMonitor = "http://adguard.oxod.nl";
+        };
+      }
+      {
+        UptimeKuma = {
+          icon = "uptime-kuma.svg";
+          href = "http://uptime.oxod.nl";
+          description = "A fancy self-hosted monitoring tool ";
+          siteMonitor = "http://uptime.oxod.nl";
+        };
+      }
     ];
   }
 ]
