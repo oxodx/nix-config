acb4b334c0e80775e9d396284449dd76635bbbba
0x0D <0xOD@proton.me>
2026-03-27 21:55:30 +0100

    chore: Migrate to bare metal

---
+++ hosts/frieren/homepage/default.nix
@@ -0,0 +1,32 @@
+{
+  services.homepage-dashboard = {
+    enable = true;
+    package = unstable.homepage-dashboard;
+
+    # https://gethomepage.dev/latest/configs/settings/
+    settings = {
+      title = "0x0D's Homelab";
+      favicon = "https://oxod.nl/favicon.ico";
+      headerStyle = "clean";
+    };
+
+    # https://gethomepage.dev/latest/configs/bookmarks/
+    bookmarks = [ ];
+
+    # https://gethomepage.dev/latest/configs/services/
+    services = [ ];
+
+    # https://gethomepage.dev/latest/configs/service-widgets/
+    widgets = [ ];
+
+    # https://gethomepage.dev/latest/configs/kubernetes/
+    kubernetes = { };
+
+    # https://gethomepage.dev/latest/configs/docker/
+    docker = { };
+
+    # https://gethomepage.dev/latest/configs/custom-css-js/
+    customJS = "";
+    customCSS = "";
+  };
+}
