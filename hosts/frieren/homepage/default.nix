a7ba457f0bf67fc404b2e2d8983585ea0eb3806c
0x0D <0xOD@proton.me>
2026-03-27 23:02:56 +0100

    feat: Add web search

--- hosts/frieren/homepage/default.nix
+++ hosts/frieren/homepage/default.nix
@@ -6,9 +6,20 @@
     settings = import ./settings.nix; # https://gethomepage.dev/configs/settings/
     bookmarks = import ./bookmarks.nix; # https://gethomepage.dev/configs/bookmarks/
     services = [ ]; # https://gethomepage.dev/configs/services/
-    widgets = [ ]; # https://gethomepage.dev/configs/service-widgets/
-    kubernetes = { }; # https://gethomepage.dev/configs/kubernetes/
-    docker = { }; # https://gethomepage.dev/configs/docker/
+    widgets = [
+      {
+        search = {
+          provider = "duckduckgo";
+          target = "_blank";
+        };
+      }
+    ]; # https://gethomepage.dev/configs/service-widgets/
+    kubernetes = {
+      mode = "disabled";
+    }; # https://gethomepage.dev/configs/kubernetes/
+    docker = {
+      mode = "disabled";
+    }; # https://gethomepage.dev/configs/docker/
 
     # https://gethomepage.dev/configs/custom-css-js/
     customJS = "";
