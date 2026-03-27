d91426c388452ae1c9525fadecbb951a84e2c0c9
0x0D <0xOD@proton.me>
2026-03-27 13:38:49 +0100

    chore: Setup fail2ban for mc

--- hosts/homelab/minecraft/default.nix
+++ hosts/homelab/minecraft/default.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   config,
+  lib,
   ...
 }:
 {
@@ -13,6 +14,38 @@
     eula = true;
     openFirewall = true;
     dataDir = "/data/apps/minecraft/servers";
+  };
+
+  services.fail2ban = {
+    enable = true;
+    maxretry = 5;
+    bantime = "24h";
+    ignoreIP = [
+      "10.0.0.0/8"
+      "172.16.0.0/12"
+      "192.168.0.0/16"
+    ];
+    jails = {
+      sshd.enabled = false;
+      minecraft = {
+        enabled = true;
+        filter = "minecraft";
+        settings = {
+          logpath = "/data/apps/minecraft/servers/logs/latest.log";
+          port = "25581";
+          maxretry = 5;
+          findtime = "1m";
+        };
+      };
+    };
+  };
+
+  environment.etc = {
+    "fail2ban/filter.d/minecraft.conf".text = ''
+      [Definition]
+      failregex = ^<HOST>.*Lost connection$
+      ignoreregex =
+    '';
   };
 
   # Create Directories
