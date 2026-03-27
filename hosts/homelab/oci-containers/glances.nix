1b781982e7f21d73577b84eae4eef9165c5fc5c5
0x0D <0xOD@proton.me>
2026-03-27 13:48:25 +0100

    feat: Added glances

---
+++ hosts/homelab/oci-containers/glances.nix
@@ -0,0 +1,36 @@
+{ config, ... }:
+let
+  user = "glances";
+  dataDir = "/data/apps/glances";
+in
+{
+  users.groups.${user} = { };
+  users.users.${user} = {
+    group = user;
+    home = dataDir;
+    isSystemUser = true;
+  };
+
+  # Create Directories
+  # https://www.freedesktop.org/software/systemd/man/latest/tmpfiles.d.html#Type
+  systemd.tmpfiles.rules = [
+    "d ${dataDir} 0755 ${user} ${user}"
+  ];
+
+  # https://github.com/NixOS/nixpkgs/blob/nixos-25.11/nixos/modules/virtualisation/oci-containers.nix
+  virtualisation.oci-containers.containers = {
+    # check its logs via `journalctl -u podman-glances`
+    glances = {
+      hostname = "glances";
+      image = "nicolargo/glances:latest";
+      ports = [ "127.0.0.1:80:3001" ];
+      environment = {
+        GLANCES_OPT = "-w";
+      };
+      volumes = [
+        "${dataDir}:/app/data"
+      ];
+      autoStart = true;
+    };
+  };
+}
