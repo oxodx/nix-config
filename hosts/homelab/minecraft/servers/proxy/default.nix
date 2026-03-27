cad641cfac63c1681410313fdbd72ed7681cc087
0x0D <0xOD@proton.me>
2026-03-27 16:08:11 +0100

    fix: Maybe forcing it to text works

--- hosts/homelab/minecraft/servers/proxy/default.nix
+++ hosts/homelab/minecraft/servers/proxy/default.nix
@@ -7,18 +7,19 @@
     whitelist = import ../whitelist.nix;
     stopCommand = "end";
     files = {
-      "velocity.toml".value = {
-        "config-version" = "2.5";
-        bind = "0.0.0.0:25565";
-        motd = "0x0D Network";
-        online-mode = true;
-        "player-info-forwarding-mode" = "legacy";
-        servers = {
-          survival01 = "localhost:25571";
-          try = [ "survival01" ];
-        };
-        "forced-hosts" = { };
-      };
+      "velocity.toml".text = ''
+        config-version = "2.5"
+        bind = "0.0.0.0:25565"
+        motd = "0x0D Network"
+        online-mode = true
+        player-info-forwarding-mode = "legacy"
+
+        [servers]
+        survival01 = "localhost:25571"
+        try = ["survival01"]
+
+        [forced-hosts]
+      '';
     };
   };
 }
