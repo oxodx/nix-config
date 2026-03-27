8f3c0dd79732c405ca93888b4304f0e934efd52d
0x0D <0xOD@proton.me>
2026-03-27 22:16:38 +0100

    feat: Add bookmarks to homepage

---
+++ hosts/frieren/homepage/bookmarks.nix
@@ -0,0 +1,12 @@
+{
+  "About Me" = {
+    Blog = {
+      abbr = "Blog";
+      href = "https://oxod.nl";
+    };
+    Github = {
+      abbr = "GH";
+      href = "https://github.com/0x0Dx";
+    };
+  };
+}
--- hosts/frieren/homepage/default.nix
+++ hosts/frieren/homepage/default.nix
@@ -3,23 +3,12 @@
     enable = true;
     allowedHosts = "home.oxod.nl";
 
-    # https://gethomepage.dev/configs/settings/
-    settings = import ./settings.nix;
-
-    # https://gethomepage.dev/configs/bookmarks/
-    bookmarks = [ ];
-
-    # https://gethomepage.dev/configs/services/
-    services = [ ];
-
-    # https://gethomepage.dev/configs/service-widgets/
-    widgets = [ ];
-
-    # https://gethomepage.dev/configs/kubernetes/
-    kubernetes = { };
-
-    # https://gethomepage.dev/configs/docker/
-    docker = { };
+    settings = import ./settings.nix; # https://gethomepage.dev/configs/settings/
+    bookmarks = import ./bookmarks.nix; # https://gethomepage.dev/configs/bookmarks/
+    services = [ ]; # https://gethomepage.dev/configs/services/
+    widgets = [ ]; # https://gethomepage.dev/configs/service-widgets/
+    kubernetes = { }; # https://gethomepage.dev/configs/kubernetes/
+    docker = { }; # https://gethomepage.dev/configs/docker/
 
     # https://gethomepage.dev/configs/custom-css-js/
     customJS = "";
