bc29e1f9121e1f1a79141024b16adc1a07f871fe
0x0D <0xOD@proton.me>
2026-03-27 22:22:16 +0100

    fix: Add more random characters and pray

--- hosts/frieren/homepage/bookmarks.nix
+++ hosts/frieren/homepage/bookmarks.nix
@@ -1,14 +1,22 @@
 [
   {
-    "About Me" = {
-      Blog = {
-        abbr = "Blog";
-        href = "https://oxod.nl";
-      };
-      Github = {
-        abbr = "GH";
-        href = "https://github.com/0x0Dx";
-      };
-    };
+    "About Me" = [
+      {
+        Blog = [
+          {
+            abbr = "Blog";
+            href = "https://oxod.nl";
+          }
+        ];
+      }
+      {
+        Github = [
+          {
+            abbr = "GH";
+            href = "https://github.com/0x0Dx";
+          }
+        ];
+      }
+    ];
   }
 ]
