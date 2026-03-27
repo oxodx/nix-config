f91b90f48a0f7e15d1e0f8d1c08b198488ce918d
0x0D <0xOD@proton.me>
2026-03-27 22:09:22 +0100

    chore: Add layout and quicklaunch

--- hosts/frieren/homepage/default.nix
+++ hosts/frieren/homepage/default.nix
@@ -9,10 +9,32 @@
       baseUrl = "http://home.oxod.nl";
       startUrl = "http://home.oxod.nl";
       favicon = "https://oxod.nl/favicon.ico";
+      useEqualHeights = true;
       headerStyle = "clean";
       theme = "dark";
       color = "slate";
       language = "en";
+      layout = {
+        "Homelab Monitoring" = {
+          icon = "mdi-monitor-dashboard";
+          initiallyCollapsed = false;
+          tab = "Main";
+          style = "row";
+          columns = 3;
+        };
+        "Homelab Applications" = {
+          icon = "si-homepage";
+          tab = "Main";
+        };
+      };
+      quicklaunch = {
+        searchDescriptions = true;
+        hideInternetSearch = true;
+        showSearchSuggestions = true;
+        hideVisitURL = true;
+      };
+      showStats = true;
+      hideErrors = false;
     };
 
     # https://gethomepage.dev/configs/bookmarks/
