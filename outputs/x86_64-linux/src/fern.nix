d354f2c833b112b656f7fb45650a29447854be30
0x0D <0xOD@proton.me>
2026-03-29 19:44:44 +0200

    chore(fern): Add fern to outputs

---
+++ outputs/x86_64-linux/src/fern.nix
@@ -0,0 +1,41 @@
+{
+  # NOTE: the args not used in this file CAN NOT be removed!
+  # because haumea pass argument lazily,
+  # and these arguments are used in the functions like `mylib.nixosSystem`, `mylib.colmenaSystem`, etc.
+  inputs,
+  lib,
+  mylib,
+  myvars,
+  system,
+  genSpecialArgs,
+  ...
+}@args:
+let
+  name = "fern";
+  tags = [
+    name
+  ];
+  ssh-user = "root";
+
+  modules = {
+    nixos-modules =
+      (map mylib.relativeToRoot [
+        "secrets/nixos.nix"
+        "modules/nixos/server.nix"
+        "hosts/${name}"
+      ])
+      ++ [
+        { modules.secrets.server.application.enable = true; }
+        { modules.secrets.server.operation.enable = true; }
+      ];
+  };
+
+  systemArgs = modules // args;
+in
+{
+  nixosConfigurations.${name} = mylib.nixosSystem systemArgs;
+
+  colmena.${name} = mylib.colmenaSystem (systemArgs // { inherit tags ssh-user; });
+
+  packages.${name} = inputs.self.nixosConfigurations.${name}.config.formats.iso;
+}
