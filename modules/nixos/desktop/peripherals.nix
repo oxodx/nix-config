710a3aaea1fdc012566a3332ed5b9b50cc684ea4
0x0D <0xOD@proton.me>
2026-03-29 21:52:00 +0200

    fix: Try quantum 512 instead of 1024
    1024 may be too large causing underruns. 512 is a middle ground
    between the 64 that was causing xruns and 1024 that may be too slow.

--- modules/nixos/desktop/peripherals.nix
+++ modules/nixos/desktop/peripherals.nix
@@ -16,16 +16,16 @@
       "10-clock-quantum" = {
         "context.properties" = {
           "default.clock.rate" = 48000;
-          "default.clock.quantum" = 1024;
-          "default.clock.min-quantum" = 1024;
-          "default.clock.max-quantum" = 2048;
+          "default.clock.quantum" = 512;
+          "default.clock.min-quantum" = 512;
+          "default.clock.max-quantum" = 1024;
         };
       };
     };
 
     extraConfig.pipewire-pulse = {
       "10-pulse-quantum" = {
-        "pulse.min.quantum" = "1024/48000";
+        "pulse.min.quantum" = "512/48000";
       };
     };
   };
