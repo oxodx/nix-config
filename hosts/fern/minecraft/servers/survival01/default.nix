3ea0b3f9d3770b33bb876d84bce59aa3ee03cb8d
0x0D <0xOD@proton.me>
2026-03-30 14:39:17 +0200

    chore(fern): Bump viaversion

--- hosts/fern/minecraft/servers/survival01/default.nix
+++ hosts/fern/minecraft/servers/survival01/default.nix
@@ -42,13 +42,13 @@ in
       };
       "plugins/ViaVersion.jar" = pkgs.fetchurl rec {
         pname = "ViaVersion";
-        version = "5.7.2";
+        version = "5.8.0";
         url = "https://github.com/ViaVersion/${pname}/releases/download/${version}/${pname}-${version}.jar";
         hash = "sha256:4430a127f6cb21d7a52ec6a07a5dcc43ba235acc062c86330619b8de1d4958fd";
       };
       "plugins/ViaBackwards.jar" = pkgs.fetchurl rec {
         pname = "ViaBackwards";
-        version = "5.7.2";
+        version = "5.8.0";
         url = "https://github.com/ViaVersion/${pname}/releases/download/${version}/${pname}-${version}.jar";
         hash = "sha256:cbf4ce4dc132b1bcccc2abb9626ed18bb1bd116c9300657543458241b01000f7";
       };
