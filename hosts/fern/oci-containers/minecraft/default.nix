c2d0abd2ae782f64b86ebcc8f6073d4771f2d93c
0x0D <0xOD@proton.me>
2026-03-30 13:19:32 +0200

    feat(fern): Add minecraft

---
+++ hosts/fern/oci-containers/minecraft/default.nix
@@ -0,0 +1,54 @@
+{ config, ... }:
+let
+  user = "minecraft";
+  dataDir = "/data/apps/minecraft";
+in
+{
+  users.groups.${user} = { };
+  users.users.${user} = {
+    group = user;
+    home = dataDir;
+    isSystemUser = true;
+  };
+
+  systemd.tmpfiles.rules = [
+    "d ${dataDir} 0755 ${user} ${user}"
+  ];
+
+  environment."${dataDir}/compose.yaml".text = ''
+    services:
+      mc:
+        image: itzg/minecraft-server:latest
+        tty: true
+        stdin_open: true
+        ports:
+          - "25565:25565"
+        environment:
+          EULA: "TRUE"
+          TYPE: "PAPER"
+          MEMORY: "4096M"
+          USE_MEOWICE_FLAGS: "true"
+          TZ: "Europe/Amsterdam"
+          DIFFICULTY: "3"
+          SIMULATION_DISTANCE: "6"
+          VIEW_DISTANCE: "8"
+          SPAWN_PROTECTION: "0"
+        volumes:
+          - "${dataDir}:/data"
+  '';
+
+  systemd.services.minecraft = {
+    wantedBy = [ "multi-user.target" ];
+    after = [
+      "docker.service"
+      "docker.socket"
+    ];
+    path = [ pkgs.docker ];
+    script = ''
+      docker compose -f ${dataDir}/compose.yaml up
+    '';
+    restartTriggers = [
+      config.environment."${dataDir}/compose.yaml".source
+    ];
+  };
+}
