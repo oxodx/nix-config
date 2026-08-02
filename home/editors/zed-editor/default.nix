5ef8d0a5b1229a81ae77cf2e1796b650075a6532
oxod <git@oxod.nl>
2026-08-02 03:11:27 +0200

    chore(zed): bump qml extension to latest commit
    Reorganize rev/hash attributes in buildZedGrammar and
    buildZedRustExtension and add source links.

--- home/editors/zed-editor/default.nix
+++ home/editors/zed-editor/default.nix
@@ -2,25 +2,27 @@
   mySettings = import ./settings.nix;
   myLsp = import ./lsp.nix;
 
+  # https://github.com/yuja/tree-sitter-qmljs
   qmljsGrammar = pkgs.buildZedGrammar (finalAttrs: {
     name = "qmljs";
     version = "febf48a5b6928600cd8fb2a01254743af550780d";
     src = pkgs.fetchFromGitHub {
       owner = "yuja";
       repo = "tree-sitter-qmljs";
-      rev = finalAttrs.version;
       hash = "sha256-bRb5h6gBBxbzhxpCxJK8CsQ5BCtPTuKtuZesE/+mDY0=";
+      rev = finalAttrs.version;
     };
   });
 
+  # https://github.com/lkroll/zed-qml
   zedQml = pkgs.buildZedRustExtension (finalAttrs: {
     name = "qml";
-    version = "0.0.4";
+    version = "1c5badf066af33234c589fbc627029074c0a6699";
     src = pkgs.fetchFromGitHub {
       owner = "lkroll";
       repo = "zed-qml";
-      rev = "1c5badf066af33234c589fbc627029074c0a6699";
       hash = "sha256-Ggz0h69pYV8LhnyfIu035bEFmLNRCH89YdsvqgHBB9I=";
+      rev = finalAttrs.version;
     };
     cargoHash = "sha256-EtYYXMnJEogl0Q1KPyQk2m59qnzNZuRNo32/NFkDBL0=";
     grammars = [qmljsGrammar];
