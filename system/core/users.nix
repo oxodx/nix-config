33757f50b418cae61779ff81455e4e1d691700d9
oxod <git@oxod.nl>
2026-08-03 14:11:22 +0200

    feat(users): add groups for containers and devices

--- system/core/users.nix
+++ system/core/users.nix
@@ -1,10 +1,33 @@
 {pkgs, ...}: {
+  users.groups = {
+    oxod = {};
+    podman = {};
+    docker = {};
+    # For android platform tool's udev rules
+    adbusers = {};
+    adbusers = {};
+    # for openocd (embedded system development)
+    plugdev = {};
+    # misc
+    uinput = {};
+    # shared group for services that read/write the same data directory
+    # (e.g. sftpgo + transmission on aquamarine)
+    fileshare = {};
+  };
+
   users.users.oxod = {
     isNormalUser = true;
     shell = pkgs.zsh;
     extraGroups = [
-      "networkmanager"
+      "users"
       "wheel"
+      "networkmanager" # for nmtui / nm-connection-editor
+      "podman"
+      "docker"
+      "wireshark"
+      "adbusers" # android debugging
+      "libvirtd" # virt-viewer / qemu
+      "fileshare"
     ];
   };
 }
