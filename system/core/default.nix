9596c9714b2ac35333288f8921c3345913dac999
oxod <git@oxod.nl>
2026-08-03 13:41:45 +0200

    refactor(core): move i18n and locale settings to module

--- system/core/default.nix
+++ system/core/default.nix
@@ -3,22 +3,14 @@
     ./boot.nix
     ./core.nix
     ./fhs.nix
+    ./i18n.nix
     ./security.nix
     ./users.nix
   ];
 
   documentation.dev.enable = true;
 
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    supportedLocales = [
-      "en_US.UTF-8/UTF-8"
-    ];
-  };
-
   system.stateVersion = lib.mkDefault "26.05";
-
-  time.timeZone = "Europe/Amsterdam";
 
   zramSwap.enable = true;
 }
