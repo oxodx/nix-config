f3c227fc2eacc1c562ff44dfb8ae9cf45d188851
oxod <git@oxod.nl>
2026-08-12 17:24:21 +0200

    chore: move Android SDK license acceptance to system

--- home/editors/android-studio.nix
+++ home/editors/android-studio.nix
@@ -2,6 +2,4 @@
   home.packages = with pkgs; [
     android-studio
   ];
-
-  nixpkgs.config.android_sdk.accept_license = true;
 }
--- system/nix/nixpkgs.nix
+++ system/nix/nixpkgs.nix
@@ -4,6 +4,7 @@
     config.permittedInsecurePackages = [
       "olm-3.2.16"
     ];
+    config.android_sdk.accept_license = true;
     overlays = [
       inputs.zed-extensions.overlays.default
     ];
