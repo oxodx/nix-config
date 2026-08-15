c19b47796e980cdd4e9b6ca135a393cc8269e5a9
oxod <git@oxod.nl>
2026-08-15 23:52:58 +0200

    feat(homelab-01): enable openssh server

--- hosts/homelab-01/default.nix
+++ hosts/homelab-01/default.nix
@@ -15,6 +15,18 @@
 
   networking.networkmanager.enable = true;
 
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+    };
+  };
+
+  users.users.oxod.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG7W3t1Iqy/s2ztBP+f24+QvuASSpCIZfUvAtc4tc2BE oxod@laptop"
+  ];
+
   security.tpm2.enable = true;
 
   system.stateVersion = "26.05";
