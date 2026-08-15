2fe539c4b8bbacf8fdad54067c1d574193c38aef
oxod <git@oxod.nl>
2026-08-16 00:56:42 +0200

    feat: add home-assistant container config for homelab-01

---
+++ hosts/homelab-01/home-assistant.nix
@@ -0,0 +1,11 @@
+{
+  virtualisation.oci-containers.containers.home-assistant = {
+    image = "ghcr.io/home-assistant/home-assistant:stable";
+    volumes = [
+      "/var/lib/home-assistant:/config"
+      "/var/run/dbus:/run/dbus:ro"
+    ];
+    envrionment.TZ = "Europe/Amsterdam";
+    extraOptions = ["--network=host"];
+  };
+}
