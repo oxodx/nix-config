096310912294ec6e9ed4e34b7bc19501365bb9fd
oxod <git@oxod.nl>
2026-08-20 16:34:05 +0200

    fix(homelab-01): fix jellyfin service startup
    Use pkgs.writeText and ExecStartPre instead of bash heredoc with
    preStart to avoid Nix indentation issues and module conflicts.

--- hosts/homelab-01/jellyfin.nix
+++ hosts/homelab-01/jellyfin.nix
@@ -1,19 +1,22 @@
-{config, ...}: let
+{
+  config,
+  pkgs,
+  ...
+}: let
   customCss = "@import url('https://cdn.jsdelivr.net/npm/jellyskin@latest/dist/main.css');";
   jellyfinDir = config.services.jellyfin.dataDir;
-in {
-  systemd.services.jellyfin.preStart = ''
-    mkdir -p ${jellyfinDir}
-
-    cat << 'EOF' > ${jellyfinDir}/branding.xml
+  brandingXml = pkgs.writeText "branding.xml" ''
     <?xml version="1.0" encoding="utf-8"?>
     <BrandingOptions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
       <CustomCss>${customCss}</CustomCss>
     </BrandingOptions>
-    EOF
-
-    chown -R jellyfin:jellyfin ${jellyfinDir}
   '';
+in {
+  systemd.services.jellyfin.serviceConfig.ExecStartPre = [
+    "+mkdir -p ${jellyfinDir}"
+    "+cp ${brandingXml} ${jellyfinDir}/branding.xml"
+    "+chown -R jellyfin:jellyfin ${jellyfinDir}"
+  ];
 
   services.jellyfin.enable = true;
   networking.firewall.allowedTCPPorts = [8096];
