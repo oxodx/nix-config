8b639176848a0a26e4689e466b154b1bee03912e
oxod <git@oxod.nl>
2026-08-20 17:05:42 +0200

    chore: nuke jellyfin again

--- lib/default.nix
+++ lib/default.nix
@@ -1,46 +1,6 @@
 {lib, ...}: {
   relativeToRoot = lib.path.append ../.;
 
-  toXML = let
-    # Standard 2-space indentation (no Unicode non-breaking spaces)
-    indent = level: lib.concatStrings (lib.genList (_: "  ") level);
-
-    escapeText = s: builtins.replaceStrings ["&" "<" ">"] ["&amp;" "&lt;" "&gt;"] (toString s);
-
-    escapeAttr = s:
-      builtins.replaceStrings ["&" "<" ">" "'" "\""] ["&amp;" "&lt;" "&gt;" "&apos;" "&quot;"] (
-        toString s
-      );
-
-    renderAttrs = attrs:
-      lib.concatStringsSep "" (
-        lib.mapAttrsToList (name: value: " ${name}=\"${escapeAttr value}\"") attrs
-      );
-
-    render = level: elem:
-      if elem ? declaration
-      then
-        # Removed space between <?xml and renderAttrs to avoid double-spacing
-        "<?xml${renderAttrs elem.declaration}?>"
-      else if !builtins.isAttrs elem
-      then "${indent level}${escapeText (toString elem)}"
-      else let
-        tag = elem.tag;
-        attrs = elem.attrs or {};
-        children = elem.children or [];
-        attrStr = renderAttrs attrs;
-      in
-        if children == []
-        then "${indent level}<${tag}${attrStr} />"
-        # Render single text children inline (prevents newlines in CSS/text tags)
-        else if builtins.length children == 1 && !builtins.isAttrs (builtins.head children)
-        then "${indent level}<${tag}${attrStr}>${escapeText (toString (builtins.head children))}</${tag}>"
-        else "${indent level}<${tag}${attrStr}>\n${
-          lib.concatStringsSep "\n" (map (render (level + 1)) children)
-        }\n${indent level}</${tag}>";
-  in
-    elems: lib.concatStringsSep "\n" (map (render 0) elems);
-
   scanPaths = path:
     map (f: (path + "/${f}")) (
       builtins.attrNames (
