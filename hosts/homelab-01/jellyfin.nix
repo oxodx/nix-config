e0891738181233cfd167b8220ff224c698575e70
oxod <git@oxod.nl>
2026-08-23 02:46:14 +0200

    feat(homelab-01): add arr stack with vpn-routed torrenting
    - enable sonarr, radarr, prowlarr alongside jellyfin/seerr
    - run gluetun (mullvad wireguard) + qbittorrent via oci-containers
    - pin media user/group to 999 and create media dirs via tmpfiles

--- hosts/homelab-01/jellyfin.nix
+++ hosts/homelab-01/jellyfin.nix
@@ -1,24 +1,104 @@
 let
   storage = {
     media = "/mnt/media";
+    state = "/var/lib";
   };
+
+  mediaUid = 999;
+  mediaGid = 999;
 in {
+  networking.firewall = {
+    enable = true;
+    # 8080 is required to access the qBittorrent Web UI.
+    # We do NOT need to open 6881 here because the torrent traffic
+    # goes through the Mullvad VPN tunnel (tun0), not your local network interface.
+    allowedTCPPorts = [8080];
+  };
+
+  systemd.tmpfiles.rules = [
+    "d ${storage.media} 0775 media media -"
+    "d ${storage.media}/downloads 0775 media media -"
+    "d ${storage.media}/tv 0775 media media -"
+    "d ${storage.media}/movies 0775 media media -"
+  ];
+
+  users.groups.media = {
+    gid = mediaGid;
+  };
+  users.users.media = {
+    isSystemUser = true;
+    uid = mediaUid;
+    group = "media";
+    createHome = false;
+  };
+
   services = {
     jellyfin = {
       enable = true;
-      openFirewall = true;
+      openFirewall = true; # Automatically opens 8096
+      user = "media";
+      group = "media";
     };
 
     seerr = {
       enable = true;
-      openFirewall = true;
+      openFirewall = true; # Automatically opens 5055
+    };
+
+    sonarr = {
+      enable = true;
+      openFirewall = true; # Automatically opens 8989
+      user = "media";
+      group = "media";
+    };
+
+    radarr = {
+      enable = true;
+      openFirewall = true; # Automatically opens 7878
+      user = "media";
+      group = "media";
+    };
+
+    prowlarr = {
+      enable = true;
+      openFirewall = true; # Automatically opens 9696
     };
   };
 
-  users.users.media = {
-    isSystemUser = true;
-    group = "media";
-    createHome = false;
+  virtualisation.oci-containers.containers = {
+    gluetun = {
+      image = "qmcgaw/gluetun:latest";
+      extraOptions = ["--cap-add=NET_ADMIN"];
+      ports = [
+        "8080:8080" # qBittorrent Web UI exposed to host
+        "6881:6881" # Torrent TCP
+        "6881:6881/udp" # Torrent UDP
+      ];
+      volumes = [
+        "${storage.state}/gluetun:/gluetun"
+      ];
+      environmentFiles = ["/root/secrets/mullvad.env"];
+      environment = {
+        VPN_SERVICE_PROVIDER = "mullvad";
+        VPN_TYPE = "wireguard";
+        SERVER_COUNTRIES = "Netherlands";
+      };
+    };
+
+    qbittorrent = {
+      image = "linuxserver/qbittorrent:latest";
+      dependsOn = ["gluetun"];
+      extraOptions = ["--network=container:gluetun"];
+      environment = {
+        PUID = toString mediaUid;
+        PGID = toString mediaGid;
+        TZ = "Europe/Amsterdam";
+        WEBUI_PORT = "8080";
+      };
+      volumes = [
+        "${storage.state}/qbittorrent/config:/config"
+        "${storage.media}/downloads:/downloads"
+      ];
+    };
   };
-  users.groups.media = {};
 }
