37549860d914c453ee73a505e69d5a5ac7924f67
oxod <git@oxod.nl>
2026-08-23 02:16:35 +0200

    feat(homelab-01): enable seerr with media user

--- hosts/homelab-01/jellyfin.nix
+++ hosts/homelab-01/jellyfin.nix
@@ -1,6 +1,24 @@
-{
-  services.jellyfin = {
-    enable = true;
-    openFirewall = true;
+let
+  storage = {
+    media = "/mnt/media";
   };
+in {
+  services = {
+    jellyfin = {
+      enable = true;
+      openFirewall = true;
+    };
+
+    seerr = {
+      enable = true;
+      openFirewall = true;
+    };
+  };
+
+  users.users.media = {
+    isSystemUser = true;
+    group = "media";
+    createHome = false;
+  };
+  users.groups.media = {};
 }
