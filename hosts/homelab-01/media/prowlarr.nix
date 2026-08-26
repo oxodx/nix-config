b9894ff20478d1911b36e71ae16dad5690007146
oxod <git@oxod.nl>
2026-08-26 17:56:30 +0200

    feat(media): enable VPN confinement for prowlarr

--- hosts/homelab-01/media/prowlarr.nix
+++ hosts/homelab-01/media/prowlarr.nix
@@ -5,6 +5,7 @@ in {
     prowlarr = {
       enable = true;
       openFirewall = true;
+      vpn.enable = true;
       config = {
         apiKey._secret = vars.secrets.prowlarr.apiKey;
         hostConfig.password._secret = vars.secrets.prowlarr.password;
