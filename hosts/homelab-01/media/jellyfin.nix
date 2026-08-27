b0644ef13a98f3300bbe5f4cf16c63f33f0719b7
oxod <git@oxod.nl>
2026-08-27 17:36:19 +0200

    feat(jellyfin): add IAmParadox plugin repo and File Transformation plugin

--- hosts/homelab-01/media/jellyfin.nix
+++ hosts/homelab-01/media/jellyfin.nix
@@ -208,6 +208,9 @@ in {
       };
 
       system.pluginRepositories = {
+        "IAmParadox" = {
+          url = "https://www.iamparadox.dev/jellyfin/plugins/manifest.json";
+        };
         "Intro Skipper" = {
           url = "https://raw.githubusercontent.com/intro-skipper/manifest/d56c137ae182c04a894dd700c25b04c8d2eba855/10.11/manifest.json";
           hash = "sha256-ENwn7Ei3WU2REcxnFNwzF6NGFUcnH2kJ4E5TKbpcDII=";
@@ -215,6 +218,8 @@ in {
       };
 
       plugins = {
+        "File Transformation".enable = true;
+
         subbuzz = {
           enable = true;
 
