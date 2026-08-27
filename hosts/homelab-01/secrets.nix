ff5a1bb32e6e67afcd100e5be6244c6c46eb3bfd
oxod <git@oxod.nl>
2026-08-27 12:22:46 +0200

    chore(secrets): simplify sops secret declarations

--- hosts/homelab-01/secrets.nix
+++ hosts/homelab-01/secrets.nix
@@ -8,73 +8,20 @@
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 
   sops.secrets = {
-    "vpn/mullvad.conf" = {
-      mode = "0400";
-      owner = "root";
-    };
-
-    "jellyfin/api_key" = {
-      mode = "0400";
-      owner = "root";
-    };
-    "jellyfin/passwords/oxod" = {
-      mode = "0400";
-      owner = "root";
-    };
-
-    "sonarr/api_key" = {
-      mode = "0400";
-      owner = "root";
-    };
-    "sonarr/password" = {
-      mode = "0400";
-      owner = "root";
-    };
-
-    "sonarr-anime/api_key" = {
-      mode = "0400";
-      owner = "root";
-    };
-    "sonarr-anime/password" = {
-      mode = "0400";
-      owner = "root";
-    };
-
-    "radarr/api_key" = {
-      mode = "0400";
-      owner = "root";
-    };
-    "radarr/password" = {
-      mode = "0400";
-      owner = "root";
-    };
-
-    "prowlarr/api_key" = {
-      mode = "0400";
-      owner = "root";
-    };
-    "prowlarr/password" = {
-      mode = "0400";
-      owner = "root";
-    };
-
-    "lidarr/api_key" = {
-      mode = "0400";
-      owner = "root";
-    };
-    "lidarr/password" = {
-      mode = "0400";
-      owner = "root";
-    };
-
-    "qbittorrent/password" = {
-      mode = "0400";
-      owner = "root";
-    };
-
-    "seerr/api_key" = {
-      mode = "0400";
-      owner = "root";
-    };
+    "vpn/mullvad.conf" = { };
+    "jellyfin/api_key" = { };
+    "jellyfin/passwords/oxod" = { };
+    "sonarr/api_key" = { };
+    "sonarr/password" = { };
+    "sonarr-anime/api_key" = { };
+    "sonarr-anime/password" = { };
+    "radarr/api_key" = { };
+    "radarr/password" = { };
+    "prowlarr/api_key" = { };
+    "prowlarr/password" = { };
+    "lidarr/api_key" = { };
+    "lidarr/password" = { };
+    "qbittorrent/password" = { };
+    "seerr/api_key" = { };
   };
 }
