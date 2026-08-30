ee6db056a43ad310ef6680219fde4807eaab3468
oxod <git@oxod.nl>
2026-08-30 03:02:59 +0200

    chore(secrets): add rekey script

---
+++ secrets/rekey.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+AGE=/nix/store/g0s4hagmrgnz8bw5c3ra9dhiw211cazh-age-1.3.1/bin/age
+SECRETS_DIR=/home/oxod/dev/nix-config/secrets
+
+for f in "$SECRETS_DIR"/*.age; do
+  name=$(basename "$f")
+  echo "Re-encrypting $name..."
+  sudo $AGE -d -i /root/.config/age/keys.txt "$f" | $AGE -R "$SECRETS_DIR/recipients.txt" -o "$f"
+done
+echo "Done!"
