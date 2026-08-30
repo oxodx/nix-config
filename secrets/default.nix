0a3b6aef5bac02fb6ff6012ebb26bb76ab0f9fd7
oxod <git@oxod.nl>
2026-08-30 14:57:16 +0200

    style(secrets): format secrets/default.nix

--- secrets/default.nix
+++ secrets/default.nix
@@ -2,77 +2,29 @@
   inputs,
   pkgs,
   ...
-}:
-let
+}: let
   server = "age1fqkz7r00szqmnj72uuycsvxmd2p3hkqr05nncuhjfzmc2x0fnytsjw8028";
   laptop = "age1clkxk3teydq0f250s6xua4mhcx7jddp0rw65dvxeent7smurtqtq3e6yuv";
-in
-{
-  imports = [ inputs.agenix.nixosModules.default ];
+in {
+  imports = [inputs.agenix.nixosModules.default];
 
-  environment.systemPackages = [inputs.agenix.packages."${pkgs.stdenv.hostPlatform.system}".default
-  ];
+  environment.systemPackages = [inputs.agenix.packages."${pkgs.stdenv.hostPlatform.system}".default];
 
   age.secrets = {
-    "mullvad.conf".publicKeys = [
-      server
-      laptop
-    ];
-    "jellyfin-api-key".publicKeys = [
-      server
-      laptop
-    ];
-    "jellyfin-password-oxod".publicKeys = [
-      server
-      laptop
-    ];
-    "prowlarr-api-key".publicKeys = [
-      server
-      laptop
-    ];
-    "prowlarr-password".publicKeys = [
-      server
-      laptop
-    ];
-    "sonarr-api-key".publicKeys = [
-      server
-      laptop
-    ];
-    "sonarr-password".publicKeys = [
-      server
-      laptop
-    ];
-    "sonarr-anime-api-key".publicKeys = [
-      server
-      laptop
-    ];
-    "sonarr-anime-password".publicKeys = [
-      server
-      laptop
-    ];
-    "radarr-api-key".publicKeys = [
-      server
-      laptop
-    ];
-    "radarr-password".publicKeys = [
-      server
-      laptop
-    ];
-    "lidarr-api-key".publicKeys = [
-      server
-      laptop
-    ];
-    "lidarr-password".publicKeys = [
-      server
-      laptop
-    ];
-    "qbittorrent-password".publicKeys = [
-      server
-      laptop
-    ];
-    "seerr-api-key".publicKeys = [
-      server
-      laptop
-    ];
+    "mullvad.conf".publicKeys = [server laptop];
+    "jellyfin-api-key".publicKeys = [server laptop];
+    "jellyfin-password-oxod".publicKeys = [server laptop];
+    "prowlarr-api-key".publicKeys = [server laptop];
+    "prowlarr-password".publicKeys = [server laptop];
+    "sonarr-api-key".publicKeys = [server laptop];
+    "sonarr-password".publicKeys = [server laptop];
+    "sonarr-anime-api-key".publicKeys = [server laptop];
+    "sonarr-anime-password".publicKeys = [server laptop];
+    "radarr-api-key".publicKeys = [server laptop];
+    "radarr-password".publicKeys = [server laptop];
+    "lidarr-api-key".publicKeys = [server laptop];
+    "lidarr-password".publicKeys = [server laptop];
+    "qbittorrent-password".publicKeys = [server laptop];
+    "seerr-api-key".publicKeys = [server laptop];
   };
 }
