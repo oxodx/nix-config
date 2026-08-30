2a238591f3293c5624d45adefa44f4eac6d19c40
oxod <git@oxod.nl>
2026-08-30 15:01:19 +0200

    fix(secrets): separate CLI config from NixOS module
    - secrets/default.nix: agenix CLI config (public keys only)
    - hosts/homelab-01/secrets.nix: NixOS module (import agenix, define age.secrets)

--- hosts/homelab-01/secrets.nix
+++ hosts/homelab-01/secrets.nix
@@ -1,4 +1,16 @@
-{ self, ... }: {
+{
+  self,
+  inputs,
+  pkgs,
+  ...
+}:
+{
+  imports = [ inputs.agenix.nixosModules.default ];
+
+  environment.systemPackages = [
+    inputs.agenix.packages."${pkgs.stdenv.hostPlatform.system}".default
+  ];
+
   age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 
   age.secrets = {
--- secrets/default.nix
+++ secrets/default.nix
@@ -1,30 +1,27 @@
-{ inputs, pkgs, ... }:
+# This file is only used by the agenix CLI for rekeying.
+# It defines the public keys used to encrypt secrets.
 let
   server = "age1fqkz7r00szqmnj72uuycsvxmd2p3hkqr05nncuhjfzmc2x0fnytsjw8028";
   laptop = "age1clkxk3teydq0f250s6xua4mhcx7jddp0rw65dvxeent7smurtqtq3e6yuv";
+  systems = [
+    server
+    laptop
+  ];
 in
 {
-  imports = [ inputs.agenix.nixosModules.default ];
-
-  environment.systemPackages = [
-    inputs.agenix.packages."${pkgs.stdenv.hostPlatform.system}".default
-  ];
-
-  age.secrets = {
-    "mullvad.conf" = { };
-    "jellyfin-api-key" = { };
-    "jellyfin-password-oxod" = { };
-    "prowlarr-api-key" = { };
-    "prowlarr-password" = { };
-    "sonarr-api-key" = { };
-    "sonarr-password" = { };
-    "sonarr-anime-api-key" = { };
-    "sonarr-anime-password" = { };
-    "radarr-api-key" = { };
-    "radarr-password" = { };
-    "lidarr-api-key" = { };
-    "lidarr-password" = { };
-    "qbittorrent-password" = { };
-    "seerr-api-key" = { };
-  };
+  "./mullvad.conf.age".publicKeys = systems;
+  "./jellyfin-api-key.age".publicKeys = systems;
+  "./jellyfin-password-oxod.age".publicKeys = systems;
+  "./prowlarr-api-key.age".publicKeys = systems;
+  "./prowlarr-password.age".publicKeys = systems;
+  "./sonarr-api-key.age".publicKeys = systems;
+  "./sonarr-password.age".publicKeys = systems;
+  "./sonarr-anime-api-key.age".publicKeys = systems;
+  "./sonarr-anime-password.age".publicKeys = systems;
+  "./radarr-api-key.age".publicKeys = systems;
+  "./radarr-password.age".publicKeys = systems;
+  "./lidarr-api-key.age".publicKeys = systems;
+  "./lidarr-password.age".publicKeys = systems;
+  "./qbittorrent-password.age".publicKeys = systems;
+  "./seerr-api-key.age".publicKeys = systems;
 }
